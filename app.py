e3595480c95cbde950186ec691fe5a9556bf89c2
Lucas de Oliveira <lucasdeoliveira.1994@gmail.com>
2026-06-05 21:22:01 -0300

    Atualizando arquivos app.py e index.html

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ def index():
     return render_template("index.html", clima=clima, erro=erro)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
